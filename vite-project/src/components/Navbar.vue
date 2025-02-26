c9b65fe4f1eee14f52a6c87a85437853e9605243
zete122 <szollosizete2005@gmail.com>
2025-02-26 11:25:19 +0100

    ads

--- vite-project/src/components/Navbar.vue
+++ vite-project/src/components/Navbar.vue
@@ -199,9 +199,7 @@ onMounted(() => {
 
   <div class="dropdown logoutdropdown">
   <button class="btn profileicon" type="button" data-bs-toggle="dropdown" aria-expanded="false" >
-    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
-    </svg> 
+   <img src="/person-fill.svg" height="30px" width="30px" alt="">
   </button>
   <ul class="dropdown-menu"  >
     <li><RouterLink class="dropdown-item" to="/Profile" aria-current="page">Adataim</RouterLink></li>
--- vite-project/src/pages/profile.vue
+++ vite-project/src/pages/profile.vue
@@ -6,6 +6,14 @@ import { RouterLink } from 'vue-router';
 <template>
 <Navbar/>
 <div class="divecske">
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+    <label for="fileToUpload">
+  <div class="profile-pic" style="background-image: url('https://randomuser.me/api/portraits/med/men/65.jpg');">
+      <span class="glyphicon glyphicon-camera"></span>
+  </div>
+  </label>
+  <input type="File" name="fileToUpload" id="fileToUpload">
+</form>
 <form >
    <div class="mb-3 ">
     <label for="exampleInputEmail1" class="form-label">Név</label>
@@ -32,4 +40,38 @@ import { RouterLink } from 'vue-router';
     background-color: aquamarine;
     padding: 20px ;
 }   
+
+.profile-pic {
+    border-radius: 50%;
+    height: 150px;
+    width: 150px;
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: multiply;
+    vertical-align: middle;
+    text-align: center;
+    color: transparent;
+    transition: all .3s ease;
+    text-decoration: none;
+    cursor: pointer;
+}
+
+.profile-pic:hover {
+    background-color: rgba(0,0,0,.5);
+    z-index: 10000;
+    color: #fff;
+    transition: all .3s ease;
+    text-decoration: none;
+}
+
+.profile-pic span {
+    display: inline-block;
+    padding-top: 4.5em;
+    padding-bottom: 4.5em;
+}
+
+form input[type="file"] {
+          display: none;
+          cursor: pointer;
+ }
 </style>
